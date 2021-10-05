46c4608ae37e014fd02e2985dc4c02f389438668
letaames <50257798+letaames@users.noreply.github.com>
2021-10-05 01:24:52 -0700

    added answer selection formatting

--- Haunts-R-Us-Form.js
+++ Haunts-R-Us-Form.js
@@ -4,7 +4,7 @@ var currentQuestion = 0;
 
 var startButton = document.getElementById("startBtn");
 
-startButton.addEventListener('click',startGame);
+startButton.addEventListener('click', startGame);
 
 var nextButton = document.getElementById("nextBtn")
 
@@ -18,12 +18,13 @@ var finalAnswerList = [];
 
 var answerButtons = document.getElementsByClassName("answerButton");
 
+
 const questions = [
-    "What is 2+2?","What is my birthday?"
+    "What is 2+2?", "What is my birthday?"
 ];
 
 const allAnswers = [
-    [4,3,2,1],
+    [4, 3, 2, 1],
     ["June", "April", "November"]
 ];
 
@@ -40,41 +41,50 @@ function setNextQuestion() {
     if (currentQuestion < questions.length) {
         questionElement.innerText = questions[currentQuestion];
         questionElement.classList.add("fade-in");
-        answerList=allAnswers[currentQuestion];
+        answerList = allAnswers[currentQuestion];
 
-        for (var z = 0; z < answerList.length; z++) {  
-                answerButtons[z].classList.remove('hide')
-                answerButtons[z].addEventListener('click', (event) => collectAnswer(answerList.length,event.target.id));
-                answerButtons[z].innerText = answerList[z];
-            }
+        for (var z = 0; z < answerList.length; z++) {
+            answerButtons[z].classList.remove('hide')
+            answerButtons[z].addEventListener('click', (event) => collectAnswer(answerList.length, event.target.id));
+            answerButtons[z].innerText = answerList[z];
         }
-    
-    gameStarted = false;
     }
 
+    gameStarted = false;
+}
+
 function goToNext() {
+    selectedButton = document.getElementsByClassName('chosen')
+    finalAnswerList.push(selectedButton);
     currentQuestion++;
-    Array.from(answerButtons).forEach(button => { button.classList.add('hide');   
+    Array.from(answerButtons).forEach(button => {
+        button.classList.add('hide');
     });
-    Array.from(answerButtons).forEach(button => { button.classList.remove('chosen');
+    Array.from(answerButtons).forEach(button => {
+        button.classList.remove('chosen');
     });
-    Array.from(answerButtons).forEach(button => { button.classList.remove('disabled');
+    Array.from(answerButtons).forEach(button => {
+        button.classList.remove('disabled');
     });
     setNextQuestion()
 }
 
 
-        
-      
+
+
 function collectAnswer(answerListLength, buttonID) {
-    buttonInteger=buttonID.split("-")[1]
-    finalAnswerList.push(buttonInteger);
+    buttonInteger = buttonID.split("-")[1]
     for (var i = 0; i < answerListLength; i++) {
         if (i == buttonInteger) {
             answerButtons[i].classList.add('chosen');
             nextButton.classList.remove('hide');
+            answerButtons[i].classList.remove('disabled')
+        } else {
+            answerButtons[i].classList.add('disabled');
+            answerButtons[i].classList.remove('chosen');
+
         }
-        else  answerButtons[i].classList.add('disabled');
+
     }
 
 }
