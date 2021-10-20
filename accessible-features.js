7d2e158c4f71bf086fdcc05ed53c39185db12eec
letaames <50257798+letaames@users.noreply.github.com>
2021-10-19 17:08:11 -0700

    added new js to only use highlight with keys

---
+++ accessible-features.js
@@ -0,0 +1,11 @@
+// Let the document know when the mouse is being used
+document.body.addEventListener('mousedown', function() {
+    document.body.classList.add('using-mouse');
+  });
+  
+  // Re-enable focus styling when Tab is pressed
+  document.body.addEventListener('keydown', function(event) {
+    if (event.key === 9) {
+      document.body.classList.remove('using-mouse');
+    }
+  });
